e52611a26f2547e074eb0119d0c20fd651c06867
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-09 20:04:16 -0700

    Create chart_faction_002.js

--- charts/chart_faction_002.js
+++ charts/chart_faction_002.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1614771438&headers=1&range=I1:J8');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1758006414&headers=1&range=I1:J8');
 			query.send(handleQueryResponse);
       }
 
