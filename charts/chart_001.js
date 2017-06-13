df769d281e430ffbe96bc8516a00ac2d4c627088
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-12 18:07:07 -0700

    Create chart_001.js

--- charts/chart_001.js
+++ charts/chart_001.js
@@ -27,12 +27,10 @@
 // colors: ['#ff0000', '#ffd700', '#008000', '#0000ff', '#4b0082','#708090']
 
 document.addEventListener('DOMContentLoaded', function() {
-	
-	google.charts.load('current', {'packages':['corechart', 'table']});
-	google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-	 
         var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1614771438&headers=1&range=I2:O13');
 			query.send(handleQueryResponse);
       }
@@ -50,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			vAxis: {
 				title: 'Average Influence Cost'
 			}
-		};
+        	};
 
         	var chart = new google.visualization.LineChart(document.getElementById('chart_001'));
 
@@ -70,9 +68,52 @@ document.addEventListener('DOMContentLoaded', function() {
 	else {
     		window.resize = resizeChart;
 	}
-
-	}, false);
 	
+}, false);
+	document.addEventListener('DOMContentLoaded', function() {
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1614771438&headers=1&range=I2:O13');
+			query.send(handleQueryResponse);
+      }
+
+	function handleQueryResponse(response) {
+  		var data = response.getDataTable();
+
+        	var options = {
+			title: 'Average Influence Cost of Power Cost, by Faction',
+			curveType: 'function',
+			legend: { position: 'bottom' },
+			hAxis: {
+				title: 'Power Cost'
+			},
+			vAxis: {
+				title: 'Average Influence Cost'
+			}
+        	};
+
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_001'));
+
+        	chart.draw(data, options);
+	}
+	
+	function resizeChart () {
+    		chart.draw(data, options);
+	}
+	
+	if (document.addEventListener) {
+    		window.addEventListener('resize', resizeChart);
+	}
+	else if (document.attachEvent) {
+    		window.attachEvent('onresize', resizeChart);
+	}
+	else {
+    		window.resize = resizeChart;
+	}
+	
+}, false);
 
 
 
