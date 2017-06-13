b88de9e2f1dec04b8d2a5de6cbd45257492c4449
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-12 18:27:46 -0700

    Create chart_faction_001.js

--- charts/chart_faction_001.js
+++ charts/chart_faction_001.js
@@ -50,7 +50,7 @@ google.charts.load('current', {'packages':['corechart']});
 			}
         	};
 
-        	var chart = new google.visualization.LineChart(document.getElementById('chart_001'));
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_faction_001'));
 
         	chart.draw(data, options);
 	}
@@ -70,50 +70,3 @@ google.charts.load('current', {'packages':['corechart']});
 	}
 	
 }, false);
-	document.addEventListener('DOMContentLoaded', function() {
-google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1614771438&headers=1&range=I2:O13');
-			query.send(handleQueryResponse);
-      }
-
-	function handleQueryResponse(response) {
-  		var data = response.getDataTable();
-
-        	var options = {
-			title: 'Average Influence Cost of Power Cost, by Faction',
-			curveType: 'function',
-			legend: { position: 'bottom' },
-			hAxis: {
-				title: 'Power Cost'
-			},
-			vAxis: {
-				title: 'Average Influence Cost'
-			}
-        	};
-
-        	var chart = new google.visualization.LineChart(document.getElementById('chart_001'));
-
-        	chart.draw(data, options);
-	}
-	
-	function resizeChart () {
-    		chart.draw(data, options);
-	}
-	
-	if (document.addEventListener) {
-    		window.addEventListener('resize', resizeChart);
-	}
-	else if (document.attachEvent) {
-    		window.attachEvent('onresize', resizeChart);
-	}
-	else {
-    		window.resize = resizeChart;
-	}
-	
-}, false);
-
-
-
