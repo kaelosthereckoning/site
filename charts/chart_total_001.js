40a686af2ba92378f56e4db157275ab55a0f9ffe
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-13 16:51:53 -0700

    Create chart_total_001.js

---
+++ charts/chart_total_001.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1758006414&headers=1&range=I1:J8');
+			query.send(handleQueryResponse);
+      }
+
+	function handleQueryResponse(response) {
+  		var data = response.getDataTable();
+
+        	var options = {
+			title: 'Total Card Availability, by Faction',
+			legend: { position: 'bottom' },
+			pieSliceText: 'label',
+        	};
+
+        	var chart = new google.visualization.PieChart(document.getElementById('chart_total_001'));
+
+        	chart.draw(data, options);
+	}
+	
+	function resizeChart () {
+    		chart.draw(data, options);
+	}
+	
+	if (document.addEventListener) {
+    		window.addEventListener('resize', resizeChart);
+	}
+	else if (document.attachEvent) {
+    		window.attachEvent('onresize', resizeChart);
+	}
+	else {
+    		window.resize = resizeChart;
+	}
+	
+}, false);
