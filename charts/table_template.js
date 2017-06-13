1ccca79c101faeaadb6a9dedd0ed802f2e49ef20
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-12 19:49:22 -0700

    Create table_template.js

---
+++ charts/table_template.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawTable() {
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=1878162950&headers=1&range=A3:D');
+			query.send(handleQueryResponse);
+      }
+
+	function handleQueryResponse(response) {
+  		var data = response.getDataTable();
+
+        	var options = {
+			title: 'Influence Required for 90% Odds of Playing Card on Curve',
+			showRowNumber: true, 
+      sort: 'enable',
+      sortAscending: false,
+      sortColumn: 3,
+        	};
+
+        	var chart = new google.visualization.Table(document.getElementById('table_001'));
+
+        	table.draw(data, options);
+	}
+	
+	function resizeChart () {
+    		chart.draw(data, options);
+	}
+	
+	if (document.addEventListener) {
+    		window.addEventListener('resize', resizeChart);
+	}
+	else if (document.attachEvent) {
+    		window.attachEvent('onresize', resizeChart);
+	}
+	else {
+    		window.resize = resizeChart;
+	}
+	
+}, false);
