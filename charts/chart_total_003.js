945362de9f3bccffa8efae9517ae101eaec38e95
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-21 16:17:03 -0700

    Update and rename chart_total_002.js to chart_total_003.js

--- charts/chart_total_003.js
+++ charts/chart_total_003.js
@@ -50,7 +50,7 @@ google.charts.load('current', {'packages':['corechart']});
 			}
         	};
 
-        	var chart = new google.visualization.LineChart(document.getElementById('chart_total_002'));
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_total_003'));
 
         	chart.draw(data, options);
 	}
