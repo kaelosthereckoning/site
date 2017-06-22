7326f723a6375a0c1a6456d3cdd3054969e31c08
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-06-22 16:51:32 -0700

    Update table_power_002.js

--- charts/table_power_002.js
+++ charts/table_power_002.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['table']});
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16_ICRavvwQN_qJSHuj3CZMDsTFZAHPuD1Oy7mndEyKs/gviz/tq?gid=561468936&headers=1&range=A2:F12');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheetsd/1M7sbpFIlvchRQ4CYE-S5EifDXltz5sBFO2vPNqjce9Q/gviz/tq?gid=561468936&headers=1&range=A2:F12');
 			query.send(handleQueryResponse);
       }
 
