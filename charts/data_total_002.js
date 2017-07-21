c577677bbe5ca0050392f200e9a094a5a124e9cd
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-20 23:25:31 -0700

    Update data_total_002.js

--- charts/data_total_002.js
+++ charts/data_total_002.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['table']});
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1M7sbpFIlvchRQ4CYE-S5EifDXltz5sBFO2vPNqjce9Q/gviz/tq?gid=1727656933&headers=1&range=A3:H18');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1M7sbpFIlvchRQ4CYE-S5EifDXltz5sBFO2vPNqjce9Q/gviz/tq?gid=1727656933&headers=1&range=A3:G18');
 			query.send(handleQueryResponse);
       }
 
