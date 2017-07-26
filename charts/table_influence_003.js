12b5aed4230268c87678b79b757e055e5b1fb706
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-26 09:51:28 -0700

    Update and rename table_power_003.js to table_influence_003.js

--- charts/table_influence_003.js
+++ charts/table_influence_003.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['table']});
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1M7sbpFIlvchRQ4CYE-S5EifDXltz5sBFO2vPNqjce9Q/gviz/tq?gid=696537664&headers=1&range=A2:G');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1M7sbpFIlvchRQ4CYE-S5EifDXltz5sBFO2vPNqjce9Q/gviz/tq?gid=696537664&headers=1&range=B2:G');
 			query.send(handleQueryResponse);
       }
 
