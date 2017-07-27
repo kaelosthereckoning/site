d2b4226dc0a2d28e28e446698fb9694d335872d6
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-27 16:32:34 -0700

    Create chart_attachments_003.js

---
+++ charts/chart_attachments_003.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/19AZiVnrB5nB5ttqOJbf6r3_acPDjEpfSn9JAn-qj5lg/gviz/tq?gid=144671595&headers=1&range=A32:P44');
+			query.send(handleQueryResponse);
+      }
+
+	function handleQueryResponse(response) {
+  		var data = response.getDataTable();
+
+        	var options = {
+			title: 'Attachment Availability per Power Cost, by Deck Type',
+			curveType: 'function',
+			legend: { position: 'bottom' },
+			hAxis: {
+				title: '# Attachments'
+			},
+			vAxis: {
+				title: 'Power Cost'
+			}
+        	};
+
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_attachments_003'));
+
+        	chart.draw(data, options);
+	}
+	
+	function resizeChart () {
+    		chart.draw(data, options);
+	}
+	
+	if (document.addEventListener) {
+    		window.addEventListener('resize', resizeChart);
+	}
+	else if (document.attachEvent) {
+    		window.attachEvent('onresize', resizeChart);
+	}
+	else {
+    		window.resize = resizeChart;
+	}
+	
+}, false);
