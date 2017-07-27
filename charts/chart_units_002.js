1419c10c56b65aa3ac21d153b554f08125ced86d
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-27 16:25:37 -0700

    Create chart_units_002.js

---
+++ charts/chart_units_002.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1jUyXuP_IHkbge8Gw-Ev12K1qKoAg7eyrRx3NikLkUyk/gviz/tq?gid=1459661057&headers=1&range=A17:R29');
+			query.send(handleQueryResponse);
+      }
+
+	function handleQueryResponse(response) {
+  		var data = response.getDataTable();
+
+        	var options = {
+			title: 'Unit Availability per Power Cost, by Faction',
+			curveType: 'function',
+			legend: { position: 'bottom' },
+			hAxis: {
+				title: '# Units'
+			},
+			vAxis: {
+				title: 'Power Cost'
+			}
+        	};
+
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_units_002'));
+
+        	chart.draw(data, options);
+	}
+	
+	function resizeChart () {
+    		chart.draw(data, options);
+	}
+	
+	if (document.addEventListener) {
+    		window.addEventListener('resize', resizeChart);
+	}
+	else if (document.attachEvent) {
+    		window.attachEvent('onresize', resizeChart);
+	}
+	else {
+    		window.resize = resizeChart;
+	}
+	
+}, false);
