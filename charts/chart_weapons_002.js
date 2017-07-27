bf6dd11b24ed9e48501961f77047c5b37569750d
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-27 16:59:51 -0700

    Update chart_weapons_002.js

--- charts/chart_weapons_002.js
+++ charts/chart_weapons_002.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/19AZiVnrB5nB5ttqOJbf6r3_acPDjEpfSn9JAn-qj5lg/gviz/tq?gid=144671595&headers=1&range=A17:R29');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/19AZiVnrB5nB5ttqOJbf6r3_acPDjEpfSn9JAn-qj5lg/gviz/tq?gid=1570529661&headers=1&range=A17:R29');
 			query.send(handleQueryResponse);
       }
 
@@ -11,18 +11,18 @@ google.charts.load('current', {'packages':['corechart']});
   		var data = response.getDataTable();
 
         	var options = {
-			title: 'Attachment Availability per Power Cost, by Faction',
+			title: 'WEapon Availability per Power Cost, by Faction',
 			curveType: 'function',
 			legend: { position: 'bottom' },
 			hAxis: {
-				title: '# Attachments'
+				title: '# Weapons'
 			},
 			vAxis: {
 				title: 'Power Cost'
 			}
         	};
 
-        	var chart = new google.visualization.LineChart(document.getElementById('chart_attachments_002'));
+        	var chart = new google.visualization.LineChart(document.getElementById('chart_weapons_002'));
 
         	chart.draw(data, options);
 	}
