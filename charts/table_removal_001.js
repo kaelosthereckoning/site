3ecc3ffc5fa08c3992956b4e3759a23060095708
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-27 18:12:16 -0700

    Update table_removal_001.js

--- charts/table_removal_001.js
+++ charts/table_removal_001.js
@@ -3,7 +3,7 @@ google.charts.load('current', {'packages':['table']});
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1srtOh6iyHjGV1Wnw3OiHmJxV7c1cC2BKHBec5uFmdiY/gviz/tq?gid=1659149536&headers=1&range=A16:B28');
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1srtOh6iyHjGV1Wnw3OiHmJxV7c1cC2BKHBec5uFmdiY/gviz/tq?gid=1659149536&headers=1&range=A16:B29');
 			query.send(handleQueryResponse);
       }
 
