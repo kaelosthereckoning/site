ea103927626b83b745df60348de8d892e080900d
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-27 18:38:28 -0700

    Update and rename table_type_001.js to table_type_000.js

--- charts/table_type_000.js
+++ charts/table_type_000.js
@@ -18,7 +18,7 @@ google.charts.load('current', {'packages':['table']});
       			//sortColumn: 3,
         	};
 
-        	var table = new google.visualization.Table(document.getElementById('table_type_001'));
+        	var table = new google.visualization.Table(document.getElementById('table_type_000'));
 
         	table.draw(data, options);
 	}
