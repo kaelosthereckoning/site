b2f4b33c5c4d94147b88aeecf978ab9c808266b3
Kaelos: The Reckoning <kaelosthereckoning@gmail.com>
2017-07-29 18:27:49 -0700

    Update and rename chart_deck-type_001.js to chart_total_003.js

--- charts/chart_total_003.js
+++ charts/chart_total_003.js
@@ -16,7 +16,7 @@ google.charts.load('current', {'packages':['corechart']});
 			pieSliceText: 'label',
         	};
 
-        	var chart = new google.visualization.PieChart(document.getElementById('chart_deck-type_001'));
+        	var chart = new google.visualization.PieChart(document.getElementById('chart_total_003'));
 
         	chart.draw(data, options);
 	}
